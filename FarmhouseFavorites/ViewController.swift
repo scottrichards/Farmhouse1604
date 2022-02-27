7adbc14981a5ebb125bfd19b4e5a5343eadfe1d6
Scott Richards <scott.richards@glassdoor.com>
2022-02-27 10:11:35 +0100

    Initial Commit

---
+++ FarmhouseFavorites/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FarmhouseFavorites
+//
+//  Created by Scott Richards on 2/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
