de6b0d98b4d52f84e44167374e42f34c3c7a5f78
Scott Richards <scott.richards@glassdoor.com>
2022-02-27 18:23:10 +0100

    Fixes

---
+++ FarmhouseFavorites/View Controllers/Main/MainHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  MainHeaderView.swift
+//  FarmhouseFavorites
+//
+//  Created by Scott Richards on 2/27/22.
+//
+
+import UIKit
+
+class MainHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- FarmhouseFavorites/View Controllers/Main/MainTableViewController.swift
+++ FarmhouseFavorites/View Controllers/Main/MainTableViewController.swift
@@ -28,12 +28,6 @@ class MainTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
@@ -60,7 +54,19 @@ class MainTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 380.0
+        if indexPath.section == 0 {
+            return UITableView.automaticDimension
+        } else {
+            return 380.0
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 0 {
+                return UITableView.automaticDimension
+            } else {
+                return 380.0
+            }
     }
 
     
